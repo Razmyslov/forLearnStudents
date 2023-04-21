c143883ef5ad9a002c25754e3b3a996c900e403f
reabletop <razmyslov.ivan.2004@gmail.com>
2023-04-21 12:12:41 +0300

    upd

---
+++ blog-js/app.js
@@ -0,0 +1,46 @@
+const allPosts = [
+    {title: 'postN1', description: 'desc1'},
+];
+
+const posts = document.querySelector('div.posts');
+
+window.onload = generateAllPosts;
+
+function generateAllPosts(){
+    let data = '';
+
+    for( let post of allPosts){
+        data +=`
+            <div class="post">
+                <p class="title"> ${post.title} </p>
+                <p class="description"> ${post.description} </p>
+                <div class="navigate">
+                    <button class="desc">Описание</button>
+                    <button class="delete">Удалить</button>
+                </div>
+            </div>
+        `
+    }
+
+    posts.innerHTML = data;
+
+}
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const input = document.getElementById('input')
+    const textarea = document.getElementById('textarea')
+
+    let title = input.value;
+    let description = textarea.value;
+
+    allPosts.push({
+        title: title,
+        description: description
+    });
+
+    generateAllPosts();
+})
