eeb6e7af46084bdad4da4957994e3f0246c2b4d3
reabletop <razmyslov.ivan.2004@gmail.com>
2023-05-26 15:12:25 +0300

    upd

--- todo/210/script.js
+++ todo/210/script.js
@@ -1,14 +1,7 @@
 const tasks = {
-    all: [
-        {id:1, title: 'Title1', description: 'Description 1', startDate: '', endDate:''},
-        {id:2, title: 'Title2', description: 'Description 2', startDate: '', endDate:''},
-        {id:3, title: 'Title3', description: 'Description 3', startDate: '', endDate:''},
-    ],
+    all: [{id:1, title: 'Title1', description: 'Description 1', startDate: '', endDate:''},{id:2, title: 'Title2', description: 'Description 2', startDate: '', endDate:''},{id:3, title: 'Title3', description: 'Description 3', startDate: '', endDate:''}],
     complete: [],
-    overdue: [
-        {id:9, title: 'Title9', description: 'Description 9', startDate: '', endDate:''},
-        {id:10, title: 'Title10', description: 'Description 10', startDate: '', endDate:''},
-    ]
+    overdue: [{id:9, title: 'Title9', description: 'Description 9', startDate: '', endDate:''},{id:10, title: 'Title10', description: 'Description 10', startDate: '', endDate:''}]
 }		
 
 // set(tasks)
@@ -45,8 +38,10 @@ function generateTasks(type){
     const tasks = get()[type];
 
     let html = ''
-    
-    console.log(type, tasks);
+
+    const navigation = type === 'all'
+        ? '<button class="button-complete green">Завершено</button><button class="button-delete red">Удалить</button>'
+        : '<button class="button-delete red">Удалить</button>'
 
     for(let task of tasks){
         html += `
@@ -61,13 +56,35 @@ function generateTasks(type){
                     </p>
                 </div>
                 <div class="navigate">
-                    <button class="button-complete green">Завершено</button>
-                    <button class="button-delete red">Удалить</button>
+                    ${navigation}
                 </div>
             </div>
         `
     }
+    
+    const titleTasks = document.getElementById('title-tasks')
+    const classTitleTasks = titleTasks.classList[1];
+    titleTasks.classList.remove(classTitleTasks);
+
+    const nameTitleTasks = {
+        all: 'Незавершенные задачи',
+        complete: 'Заврешенные задачи',
+        overdue: 'Просроченные задачи'
+    }
+
+    titleTasks.textContent = nameTitleTasks[type];
+    titleTasks.classList.add(`title-${type}`);
+
+    if(tasks.length === 0){
+        html = `
+            <div class='not-found-tasks'>
+                <h2>Задачи не найдены</h2>
+            </div>    
+        `
+    }
+
     placeTasks.innerHTML = html;
+
 }
 
 window.onload = generateTasks('all')
