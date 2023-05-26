aca9a570931e7e909095d79568fcc86aae3827c9
reabletop <razmyslov.ivan.2004@gmail.com>
2023-05-26 13:58:09 +0300

    upd 210

--- todo/210/script.js
+++ todo/210/script.js
@@ -1,3 +1,18 @@
+const tasks = {
+    all: [
+        {id:1, title: 'Title1', description: 'Description 1', startDate: '', endDate:''},
+        {id:2, title: 'Title2', description: 'Description 2', startDate: '', endDate:''},
+        {id:3, title: 'Title3', description: 'Description 3', startDate: '', endDate:''},
+    ],
+    complete: [],
+    overdue: [
+        {id:9, title: 'Title9', description: 'Description 9', startDate: '', endDate:''},
+        {id:10, title: 'Title10', description: 'Description 10', startDate: '', endDate:''},
+    ]
+}		
+
+// set(tasks)
+
 if(!localStorage.getItem('tasks')){
     localStorage.setItem('tasks', JSON.stringify({
         all: [],
@@ -21,7 +36,8 @@ const placeTasks = document.querySelector('main div.tasks');
 const navButtons = document.querySelectorAll('header nav button');
 navButtons.forEach( button  => {
     button.addEventListener('click', event => {
-        generateTasks(event.srcElement.id.split('-')[1])
+        const type = event.srcElement.id.split('-')[1]
+        generateTasks(type)
     })
 })
 
@@ -30,6 +46,8 @@ function generateTasks(type){
 
     let html = ''
     
+    console.log(type, tasks);
+
     for(let task of tasks){
         html += `
             <div class="task ${type}" id="task-id-${task.id}">
