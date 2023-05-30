2358cad9d17946d06eab310a91a445ab356aa24e
reabletop <razmyslov.ivan.2004@gmail.com>
2023-05-30 18:02:21 +0300

    upd

--- todo/209/script.js
+++ todo/209/script.js
@@ -1,24 +1,13 @@
-const tasks = {
-    all: [
-        {id:1, title: 'Title1', description: 'Description 1', startDate: '', endDate:''},
-        {id:2, title: 'Title2', description: 'Description 2', startDate: '', endDate:''},
-        {id:3, title: 'Title3', description: 'Description 3', startDate: '', endDate:''},
-    ],
-    complete: [],
-    overdue: [
-        {id:9, title: 'Title9', description: 'Description 9', startDate: '', endDate:''},
-        {id:10, title: 'Title10', description: 'Description 10', startDate: '', endDate:''},
-    ]
-}		
-
-// set(tasks)
-
 if(!localStorage.getItem('tasks')){
     localStorage.setItem('tasks', JSON.stringify({
         all: [],
         complete: [],
         overdue: []
     }))
+}
+
+if(!localStorage.getItem('id')){
+    localStorage.setItem('id', JSON.stringify( 1 ))
 }
 
 function get(){
@@ -136,41 +125,83 @@ function generateTasks(type){
     const addTask = document.querySelector('div.add-task')
     openShowForm.addEventListener('click', () => {
         addTask.style.display = 'flex';
-        openShowForm.style.display = 'none';
     });
 
     document.getElementById('exitForm').addEventListener('click', () => {
         addTask.style.display = 'none';
-        openShowForm.style.display = 'block';
     })
 
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        
-        const inputs = document.querySelectorAll('form input');
-        let task = {
-            title: '',
-            description: '',
-            startDate: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
-            endDate: ''
-        }
-        
-        inputs.forEach(input => {
-            if(input.id === 'endDate'){
-                task.endDate = input.value.split('-').reverse().join('.')
-            } else if(input.id === 'endDateTime') {
-                task.endDate += ` ${input.value}`
-            }else {
-                task[input.id] += input.value
-            }
-        })
-
-        console.log(task);
-
-    })
-
+    checkOverdue();
 }
+
+
+let id = 1;
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const inputs = document.querySelectorAll('form input');
+    let task = {
+        title: '',
+        description: '',
+        startDate: new Date().toLocaleDateString() + '.' + new Date().toLocaleTimeString(),
+        endDate: ''
+    }
+    
+    inputs.forEach(input => {
+        if(input.id === 'endDate'){
+            task.endDate = input.value.split('-').reverse().join('.')
+        } else if(input.id === 'endDateTime') {
+            task.endDate += `.${input.value}`
+        }else {
+            task[input.id] += input.value
+        }
+    });
+
+    inputs.forEach( input => input.value = '' );
+
+    let id = JSON.parse( localStorage.getItem('id') )
+
+    task.id = id;
+
+    localStorage.setItem('id', JSON.stringify(++id))
+
+    update('all', task);
+
+    generateTasks('all');
+
+    document.querySelector('div.add-task')
+        .style.display = 'none';
+})
 
 window.onload = generateTasks('all');
 
+function checkOverdue(){
+    const allTasks = get();
+
+    for(let task of allTasks['all']){
+        const date = new Date().toLocaleDateString('ru-Ru')
+            + '.' + new Date().toLocaleTimeString();
+        console.log(date)
+
+        const day = [ date.split('.')[0], task.endDate.split('.')[0] ]
+        const mounth = [ date.split('.')[1], task.endDate.split('.')[1] ]
+        const year = [ date.split('.')[2], task.endDate.split('.')[2] ]
+        const time = [ date.split('.')[3], task.endDate.split('.')[3] ]
+
+        if(day[0] < day[1] || mounth[0] < mounth[1] || year[0] < year[1] || time[0] < time[1]){
+            const index = allTasks['all']
+                .findIndex(obj => obj.id === task.id)
+            
+            allTasks['overdue'].push(task);
+            allTasks['all'].splice(index, 1);
+        }
+
+    }
+
+    set(allTasks);
+    
+    setInterval(()=>{
+        checkOverdue()
+    },30000)
+}
